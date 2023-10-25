6a4b7d7361c95c439ea9995defc950bdc6608424
Masoud-M <masoudmoghaddam999@gmail.com>
2023-10-26 00:39:59 +0330

    added a backend using supabase service and tested it via useEffect in cabins page
    unfortunately can't enable RLS in supabase and if someone finds the public key, they have full access to the server :D

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,18 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(function () {
+    getCabins().then((data) => console.log(data));
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src="https://bbcprcrjaxmudkhtxpnr.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2023-10-25T21%3A04%3A06.771Z" />
     </Row>
   );
 }
