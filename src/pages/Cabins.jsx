fbcd8cd3ea5d330b9d4c5947a981ba2ffe14a8fe
Masoud-M <masoudmoghaddam999@gmail.com>
2023-10-26 13:40:45 +0330

    fetching cabin data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,19 +1,19 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img src="https://bbcprcrjaxmudkhtxpnr.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2023-10-25T21%3A04%3A06.771Z" />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
