4ddc73d72a4868ca9424a1426c6b62bb3e273dec
Masoud-M <masoudmoghaddam999@gmail.com>
2023-11-03 10:29:10 +0330

    refactored the modal to a compound component and added a way to show the CabinTable in the cabins page

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,3 +1,4 @@
+import { cloneElement, createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
@@ -51,21 +52,54 @@ const Button = styled.button`
   }
 `;
 
-function Modal({ children, onClose }) {
-  // using createPortal we can render our element anywhere in the dom
+const ModalContext = createContext();
+
+function Modal({ children }) {
+  const [openName, setOpenName] = useState("");
+
+  function close() {
+    setOpenName("");
+  }
+
+  function open(name) {
+    setOpenName(name);
+  }
+
+  return (
+    <ModalContext.Provider value={{ openName, close, open }}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(ModalContext);
+  // if we would have just returned the children then we didn't have a way to pass the onClick handler to it, so instead we clone the children using the React function cloneElement and adding the props we want to pass to it
+  return cloneElement(children, { onClick: () => open(opensWindowName) });
+}
+
+function Window({ children, name }) {
+  const { openName, close } = useContext(ModalContext);
+  if (name !== openName) return null;
+
+  // using createPortal we can render our element anywhere in the dom tree while still keeping it in the position in the component tree
   return createPortal(
     <Overlay>
       <StyledModal>
-        <Button onClick={onClose}>
+        <Button onClick={close}>
           <HiXMark />
         </Button>
 
-        <div>{children}</div>
+        {/* we pass the onCloseModal prop to the children which is our CreateCabinForm, with React cloneElement function */}
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
       </StyledModal>
     </Overlay>,
     // specifying the node we want our element to be rendered at as the second argument
     document.body
   );
 }
+
+Modal.Open = Open;
+Modal.Window = Window;
 
 export default Modal;
