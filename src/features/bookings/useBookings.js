8820292c68fdc7dee9dbf1a4041f5292cd6e9264
Masoud-M <masoudmoghaddam999@gmail.com>
2023-11-10 17:09:16 +0330

    finished pagination functionality

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -24,16 +24,22 @@ export function useBookings() {
   const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
+
+  // PAGINATION
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
+  // setting data's default value to an empty object cuz otherwise it will be undefined at first and cuz a bug
   const {
     isLoading,
-    data: bookings,
+    data: { data: bookings, count } = {},
+    // data: bookings,
     error,
   } = useQuery({
     // filter and sortBy values in the queryKey are used like dependency array in useEffect, and it will make the reactQuery to reload the page
-    queryKey: ["bookings", filter, sortBy],
+    queryKey: ["bookings", filter, sortBy, page],
     // queryFn needs to return a promise so we could simply use a fetch() function but we are gonna use our own function here.
-    queryFn: () => getBookings({ filter, sortBy }),
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-  return { isLoading, bookings, error };
+  return { isLoading, bookings, error, count };
 }
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,13 +1,16 @@
+import { PAGE_SIZE } from "../utils/constants";
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   // with specifying the cabins(name) and guests(fullName, email) keywords in the select method, supabase will return all those columns data for each booking id
   // we could have used select("*, cabins(*), guests(*)") but it would have returned all the data that might have not be necessary
+  // the secund argument that we passed into the select as an object with the key value pair of count: "exact" will return the number of the results from supabase, instead of querying the whole data to get the length of it
   let query = supabase
     .from("bookings")
     .select(
-      "id, created_at, startDate, endDate, numNights, numGuests, status,totalPrice, cabins(name), guests(fullName, email)"
+      "id, created_at, startDate, endDate, numNights, numGuests, status,totalPrice, cabins(name), guests(fullName, email)",
+      { count: "exact" }
     );
 
   // FILTER
@@ -18,15 +21,20 @@ export async function getBookings({ filter, sortBy }) {
     query = query.order(sortBy.field, {
       ascending: sortBy.direction === "asc",
     });
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+    query = query.range(from, to);
+  }
 
-  const { data, error } = await query;
+  const { data, error, count } = await query;
 
   if (error) {
     console.error(error);
     throw new Error("Bookings could not be loaded");
   }
 
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
