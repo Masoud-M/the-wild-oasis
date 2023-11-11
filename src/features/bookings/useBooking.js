186eaa93646c169fac9d7b8c43b2e3473b78aabe
Masoud-M <masoudmoghaddam999@gmail.com>
2023-11-11 10:12:15 +0330

    added single page for each booking to show their details, and its using bookingId param to navigate to the related page and show the data

---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBooking } from "../../services/apiBookings";
+import { useParams } from "react-router-dom";
+
+export function useBooking() {
+  const { bookingId } = useParams();
+
+  const {
+    isLoading,
+    data: booking,
+    error,
+  } = useQuery({
+    queryKey: ["booking"],
+    // queryFn needs to return a promise so we could simply use a fetch() function but we are gonna use our own functions here.
+    queryFn: () => getBooking(bookingId),
+    // by default reactQuery will try to fetch the data 3 times but it doesn't make that much sense here so we turned it off
+    retry: false,
+  });
+
+  return { isLoading, booking, error };
+}
