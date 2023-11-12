25f8609cc8a69dd137161acb5def7ee0e09b5e6a
Masoud-M <masoudmoghaddam999@gmail.com>
2023-11-12 13:10:01 +0330

    added check out functionality to the bookings page

---
+++ src/features/check-in-out/useCheckout.js
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateBooking } from "../../services/apiBookings";
+import toast from "react-hot-toast";
+
+export function useCheckout() {
+  const queryClient = useQueryClient();
+
+  const { mutate: checkout, isLoading: isCheckingOut } = useMutation({
+    mutationFn: (bookingId) =>
+      updateBooking(bookingId, {
+        status: "checked-out",
+      }),
+
+    // the data is the returned value of mutationFn
+    onSuccess: (data) => {
+      toast.success(`Booking #${data.id} successfully checked out`);
+
+      // we can pass in the query key to invalidate or we can pass an object with property active to invalidate all the queries that are active at the moment
+      queryClient.invalidateQueries({ active: true });
+    },
+
+    onError: () => toast.error("There was an error while checking out"),
+  });
+
+  return { checkout, isCheckingOut };
+}
