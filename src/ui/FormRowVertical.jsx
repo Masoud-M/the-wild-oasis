980cdda5a68e9763cd2b4296171453a693b52fc1
Masoud-M <masoudmoghaddam999@gmail.com>
2023-11-18 10:59:20 +0330

    added login page ui

---
+++ src/ui/FormRowVertical.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+const StyledFormRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  padding: 1.2rem 0;
+`;
+
+const Label = styled.label`
+  font-weight: 500;
+`;
+
+const Error = styled.span`
+  font-size: 1.4rem;
+  color: var(--color-red-700);
+`;
+
+function FormRowVertical({ label, error, children }) {
+  return (
+    <StyledFormRow>
+      {/* since we only have one children all the time and it is the input element and we need the id attribute of the input element, we can access it through the children.props.id */}
+      {label && <Label htmlFor={children.props.id}>{label}</Label>}
+      {children}
+      {error && <Error>{error}</Error>}
+    </StyledFormRow>
+  );
+}
+
+export default FormRowVertical;
