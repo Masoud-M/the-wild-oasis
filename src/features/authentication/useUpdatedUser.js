9b04cbe6c52e5e08da5d806553e203f1cae38fb6
Masoud-M <masoudmoghaddam999@gmail.com>
2023-11-22 11:43:55 +0330

    fixed the bug that after changing the avatar, a manual reload was needed in order to see the changes.
    the query key should have been inside of an array!
    in the apiAuth file we forgot to await in the avatar changing section

--- src/features/authentication/useUpdatedUser.js
+++ src/features/authentication/useUpdatedUser.js
@@ -6,10 +6,10 @@ export function useUpdateUser() {
   const queryClient = useQueryClient();
   const { mutate: updateUser, isLoading: isUpdating } = useMutation({
     mutationFn: updateCurrentUser,
-    onSuccess: () => {
+    onSuccess: ({ user }) => {
       toast.success("User account successfully updated");
-      // queryClient.setQueryData("user", user);
-      queryClient.invalidateQueries({ queryKey: ["user"] });
+      queryClient.setQueryData(["user"], user);
+      // queryClient.invalidateQueries({ queryKey: ["user"] });
       // this method of invalidating the query catch doesn't work as inted and won't reload the page after updating avatar so we use another method which is to set the query manually
     },
     onError: (err) => toast.error(err.message),
