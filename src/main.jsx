2b2593407274223b7d05b535ed76111ee8d56ca5
Masoud-M <masoudmoghaddam999@gmail.com>
2023-11-27 13:12:32 +0330

    added ErrorBoundary so if the app stopes working it will show an error message instead of a white page, and user can click on a button and be redirected to the main page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ui/ErrorFallback.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      // onReset is the callback function we have access inside of FallbackComponent and it will be triggered after clicking on the button
+      // we are not using react router to redirect the user to the main page
+      onReset={() => window.location.replace("/")}
+    >
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
